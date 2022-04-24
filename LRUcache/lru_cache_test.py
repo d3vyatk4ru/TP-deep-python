694bec1c3f7fc74955df9b18f7dd25f3e860d3ef
d3vyatk4ru <danya.devyatkin@mail.ru>
2022-04-24 18:29:40 +0300

    added some tests

--- LRUcache/lru_cache_test.py
+++ LRUcache/lru_cache_test.py
@@ -72,6 +72,55 @@ class LRUCacheTest(unittest.TestCase):
 
         self.assertEqual(str(cache), ' -> [k1, val1] -> [k0, val0] ->')
 
+    def test_crowding_out(self):
+
+        size = 2
+
+        cache = LRUCache(size)
+
+        for i in range(size):
+            cache.set(f'k{i}', f'val{i}')
+
+        self.assertEqual(cache.get('k0'), 'val0')
+        self.assertEqual(cache.get('k1'), 'val1')
+
+        for i in range(size, 2 * size):
+            cache.set(f'k{i}', f'val{i}')
+
+        self.assertEqual(cache.get('k0'), None)
+        self.assertEqual(cache.get('k1'), None)
+
+        self.assertEqual(cache.get('k2'), 'val2')
+        self.assertEqual(cache.get('k3'), 'val3')
+
+    def test_from_task(self):
+
+        cache = LRUCache(2)
+
+        self.assertTrue(cache.set('k1', 'val1'))
+        self.assertTrue(cache.set('k2', 'val2'))
+
+        self.assertEqual(cache.get('k3'), None)
+        self.assertEqual(cache.get('k2'), 'val2')
+        self.assertEqual(cache.get('k1'), 'val1')
+
+        self.assertTrue(cache.set('k3', 'val3'))
+
+        self.assertEqual(cache.get('k3'), 'val3')
+        self.assertEqual(cache.get('k2'), None)
+        self.assertEqual(cache.get('k1'), 'val1')
+
+    def test_size_1(self):
+
+        cache = LRUCache(1)
+
+        self.assertTrue(cache.set('k1', 'val1'))
+        self.assertEqual(cache.get('k1'), 'val1')
+
+        self.assertTrue(cache.set('k2', 'val2'))
+        self.assertEqual(cache.get('k2'), 'val2')
+        self.assertEqual(cache.get('k1'), None)
+
 
 if __name__ == '__main__':
     unittest.main()
